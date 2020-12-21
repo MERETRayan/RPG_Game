6174b598555788c8fe2887fb26af8f0e380ed0c5
MERETRayan <74654820+MERETRayan@users.noreply.github.com>
2020-12-21 19:26:52 +0100

    121

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+document.getElementById("poulet").addEventListener("mouseover", mouseOver3);
+document.getElementById("poulet").addEventListener("mouseout", mouseOut3);
+
+document.getElementById("dragon").addEventListener("mouseover", mouseOver2);
+document.getElementById("dragon").addEventListener("mouseout", mouseOut2);
+
 document.getElementById("fantome").addEventListener("mouseover", mouseOver);
 document.getElementById("fantome").addEventListener("mouseout", mouseOut);
 
@@ -9,9 +16,6 @@ function mouseOut() {
   document.getElementById("StatFantome").style.display ='none';
 }
 
-document.getElementById("dragon").addEventListener("mouseover", mouseOver2);
-document.getElementById("dragon").addEventListener("mouseout", mouseOut2);
-
 function mouseOver2() {
   document.getElementById("StatDragon").style.display = 'block';
 }
@@ -20,9 +24,6 @@ function mouseOut2() {
   document.getElementById("StatDragon").style.display ='none';
 }
 
-document.getElementById("poulet").addEventListener("mouseover", mouseOver3);
-document.getElementById("poulet").addEventListener("mouseout", mouseOut3);
-
 function mouseOver3() {
   document.getElementById("StatPoulet").style.display = 'block';
 }
@@ -30,3 +31,7 @@ function mouseOver3() {
 function mouseOut3() {
   document.getElementById("StatPoulet").style.display ='none';
 }
+
+pvMob = [100,250,400];
+pvPlayer = [200,150,240,300];
+mana = [50,80,40,20];
